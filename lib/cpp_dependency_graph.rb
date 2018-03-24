7c35a10db7c23945b953c18b838451adc379f8f4
Shreyas Balakrishna <shreyasbharath@users.noreply.github.com>
2018-03-24 15:11:20 +1300

    Using ostruct for args

--- lib/cpp_dependency_graph.rb
+++ lib/cpp_dependency_graph.rb
@@ -6,9 +6,9 @@ require_relative 'cpp_dependency_graph/graph_to_dot_converter'
 
 # Generate dependency graph of a project as a dot file
 module CppDependencyGraph
-  def generate(project_dir, output_file)
-    project = Project.new(project_dir)
+  def generate(args)
+    project = Project.new(args.project_dir)
     graph = DependencyGraph.new(project)
-    GraphToDotConverter.generate(graph.component_dependencies, output_file)
+    GraphToDotConverter.generate(graph.component_dependencies, args.output_file)
   end
 end
