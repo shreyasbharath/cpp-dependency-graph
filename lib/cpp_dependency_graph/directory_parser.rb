51ee400e70fc2ec753194de5f44b1817841242f2
Shreyas Balakrishna <shreyas.balakrishna@halter.co.nz>
2020-04-12 18:06:24 +1200

    fixup! Blah

--- lib/cpp_dependency_graph/directory_parser.rb
+++ lib/cpp_dependency_graph/directory_parser.rb
@@ -1,4 +1,8 @@
 module DirectoryParser
+  def fetch_all_dirs(root_dir)
+    Find.find(root_dir).select { |e| File.directory?(e) }
+  end
+
   def glob_files(path, extensions)
     path = File.join(path, File::SEPARATOR) + '*' + extensions
     Dir.glob(path).select { |entry| File.file?(entry) }.compact
